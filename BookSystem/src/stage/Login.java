06df39b61775662ce8c497b4ceb738beb2db0595
yaooah <104478428+yaooah@users.noreply.github.com>
2023-06-25 10:42:34 +0800

    Add files via upload

--- BookSystem/src/stage/Login.java
+++ BookSystem/src/stage/Login.java
@@ -1,4 +1,25 @@
 package stage;
 
-public class Login {
+import controller.LoginController;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Login extends Pane {
+
+    public Login(Stage stage) throws IOException {
+        try {
+            FXMLLoader fxmlloader = new FXMLLoader(getClass().getResource("../view/login.fxml"));
+            this.getChildren().add(fxmlloader.load());
+            ((LoginController)fxmlloader.getController()).setOldStage(stage);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
--- BookSystem/src/utils/Main.java
+++ BookSystem/src/utils/Main.java
@@ -7,21 +7,16 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import javafx.stage.WindowEvent;
+import stage.Login;
 
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("图书借阅管理系统");
-        primaryStage.setScene(new Scene(root, 800, 600));
-        primaryStage.show();
-        primaryStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
-            @Override
-            public void handle(WindowEvent event) {
-                primaryStage.close();
-            }
-        });
+    public void start(Stage stage) throws Exception{
+        Scene scene = new Scene(new Login(stage));
+        stage.setTitle("图书借阅管理系统");
+        stage.setScene(scene);
+        stage.show();
     }
 
     public static void main(String[] args) {
